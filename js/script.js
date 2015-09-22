06d1f3b3a764365c9a71a9647b35ca8044acf06b
Tim Mechtel <tim_mechtel@hotmail.com>
2015-09-21 19:54:56 -0500

    it worked again

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 // Be sure to add an IIFE to scope your work!
+(function() {
+    var aside = document.getElementsByTagName("aside");
+    for (a = 0; a < aside.length; a++) {
+        aside[0].removeAttribute("class");
+    };
 
 
 
+    var week = document.getElementById("featured-items");
+    //my json 
+    var newitems = {
+        "items": [
+            {
+                "name": "Hazelnut Cappuccino. ",
+                "image": "images/cappuccino.jpg",
+                "description": "A warm, soothing cappuccino with a touch of hazelnut to warm you as the weather turns"
+            },
+            {
+                "name": "Chocolate Cake. ",
+                "image": "images/choc-cake.jpg",
+                "description": "Our chocolate cake is rich, moist, and dark. More chocolate to love."
+            }
+            ]
+    };
+    var object = newitems.items;
+    for (i = 0; i < object.length; i++) {
+        var item = object[i],
+            name = item.name,
+            pic = item.image,
+            descrip = item.description;
 
+        var p = document.createElement("P");
+        week.appendChild(p);
+        p.innerHTML = (name + descrip);
+        var newpic = document.createElement("img");
+        week.appendChild(newpic);
+        newpic.setAttribute("src", pic);
+    }
+}())
