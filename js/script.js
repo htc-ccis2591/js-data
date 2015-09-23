a004c7f1050c142962ea40bd074565be1d3ff37f
rjgtech26 <rj2189guse@gmail.com>
2015-09-23 17:54:39 -0500

    jsData

--- js/script.js
+++ js/script.js
@@ -24,30 +24,42 @@ console.log(items);
 
 var targetArea = document.getElementById("featured-items");
 console.log(targetArea);
-   
+       var removeHide = document.getElementsByTagName("aside").item(0).removeAttribute("class");
+        
     for (i=0; i< items.length; i++){
         var lItems = items[i];
         var lName = document.createElement("h3");
         var textName = document.createTextNode(lItems.name);
         lName.appendChild(textName);
         targetArea.appendChild(lName);
-//        var array = [
-        var removeHide = document.getElementsByTagName("aside").item(0).removeAttribute("class");
+
+    
+        var createImgElement = document.createElement("img");
+        var CreateDisElement = document.createElement("p");
+        var txtDis = document.createTextNode(lItems.description)
+        createImgElement.src = (lItems.image);
+        targetArea.appendChild(createImgElement);
+       
+        CreateDisElement.appendChild(txtDis);
+        targetArea.appendChild(CreateDisElement);
+       
+        
+
         
         console.log(items);
         console.log(lName);
         console.log(textName);
         console.log(targetArea);
         console.log(removeHide);
+        console.log(createImgElement);
+        console.log(createImgElement);
+        console.log(CreateDisElement);
+        console.log(txtDis);
+        
         
     }
     
-    
-
-
-
-
-}())
+    }())
 
 
 
