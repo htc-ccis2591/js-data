ce343a5dad2b400356a4b05e04a598f0d9c30534
dechmann <tdechmann001@my.hennepintech.edu>
2016-03-02 21:37:12 -0600

    new changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Be sure to add an IIFE to scope your work!
-
-
-
-
+$(function() {
+    $.getJSON("feature.json")
+        .done( function(data){
+        $("aside").removeClass("hide");
+            for(i = 0; i < data.featured.length; i++) {
+                $("#featured-items").append("<H3>" + data.featured[i].name + "</h3>");
+                $("#featured-items").append("<p>" + data.featured[i].description + "</p>");
+                $("#featured-items").append("<img src=" + data.featured[i].image + "></img>");
+            }
+        });
+});
