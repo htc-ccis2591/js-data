1c3bba88ae216b76b5ae4bf153b680a07ad75148
David Gossell <itpro@gossellmn.com>
2016-03-05 12:11:39 -0600

    dgossell js-data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 // Be sure to add an IIFE to scope your work!
+$(function(){
+    $.getJSON('./featured.json')
+    .done(function(data){
+        // this is the HTML we'll be adding
+        var newHTML;
+        
+        // for each features object in the returned JSON data
+        $.each(data.features, function(key, val){
+            
+            // build the HTML to add for this item
+            newHTML = '<h3>' + val.name + '</h3>';
+            newHTML += '<img src = \"' + val.image + '\">';
+            newHTML += '<em>' + val.description + '</em>';
+            newHTML += '<hr>'
+            
+            // add the item in the div
+            $('#featured-items').append(newHTML);
+            
+            // console.log(newHTML);
+            
+        });
+        
+    // in the .done section so we just won't show unless data was downloaded successfully.
+    $('aside').removeClass('hide');
+        
+    });
+})
 
 
 
