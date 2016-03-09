5283493d26174eb47496bc7dcf49094ed500b426
olidi10 <mmutanga@yahoo.fr>
2016-03-09 17:36:30 -0600

    json home work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Be sure to add an IIFE to scope your work!
+$(function () {
+    var content;
+    $.getJSON("Assig.json")
+        .done(function (data) {
 
 
+            $("#featured-items").append("<p>new-items</p>");
+
+            $("aside").removeClass("hide");
+
+            for (var i = 0; i < data.menu.length; i++) {
+                content = data
+                    .show(content);
+            }
+
+        })
 
 
+});
