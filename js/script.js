e86cc671ca67d2934cb4bc11cbe8a8454949fb56
radhikaprabu <radhikaprabu91699@gmail.com>
2016-03-09 08:21:17 -0600

    json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Be sure to add an IIFE to scope your work!
-
-
-
-
+$(function () {
+	$.getJSON('items.json')
+		.done(function (data) {
+			$.each(data.featuredItem, function (key,value) {
+				$('#featured-items').append('<h3>' + value.Name + '</h3>')
+					.append('<img src="' +value.Image + '"/>')
+					.append('<p>' + value.Description + '</p>')
+			});
+			$('aside').removeClass('hide');
+				
+		});
+});
