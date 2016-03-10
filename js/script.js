f5839cef8b206dcb8736286ea0e9060784073db2
olidi10 <mmutanga@yahoo.fr>
2016-03-09 18:14:53 -0600

    HW JSON

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ $(function () {
     $.getJSON("Assig.json")
         .done(function (data) {
 
-
             $("#featured-items").append("<p>new-items</p>");
 
             $("aside").removeClass("hide");
 
             for (var i = 0; i < data.menu.length; i++) {
-                content = data
-                    .show(content);
+
+                content += '<h3>' + menu[i].Name + '</h3>';
+                content += '<img src = "' + menu[i].Image + '"/>';
+                content += '<p>' + menu[i].Description + '</p>';
+                //.show(content);
             }
 
         })
