22fcf9e81a1f6d77c6401187c6e797d61f65fc23
brianandreasen1983 <bandreasen001@my.hennepintech.edu>
2016-09-29 22:31:57 -0500

    JSON Data!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Be sure to add an IIFE or jQuery ready to scope your work!
+$(document).ready(function () {
+
+    $('aside').removeClass("hide");
+
+    var createmyMenu = function (name, image, description) {
+        var featureditems = "#featured-items";
+        
+        $(featureditems).append("<h3>" + name + "</h3>");
+        $(featureditems).append('<img src=" ' + image + '">');
+        $(featureditems).append("<p>" + description + "</p>");
+
+    }; //ends function
+    
+    $.getJSON("myMenu.json", function (data) {
+
+        $.each(data.myMenu, function (index, item) {
+            createmyMenu(item.name, item.image, item.description);
 
 
+        }); //Ends the each loop for jquery
+    }); //Ends JSON
 
-
+}); //Ends jQuery ready
